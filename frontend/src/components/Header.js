1328a3cf6ba0953ef61f13e0f05347cffb496ed1
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-03-06 12:40:08 -0700

    Header-setup

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,28 @@
+// import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Golf Proshop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser /> Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;

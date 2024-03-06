68103ccdaf6bfa662b3e9935ed6e1ff8c699f84b
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-03-06 14:00:03 -0700

    product-images-start

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,6 +1,19 @@
 const products = [
   {
     _id: "1",
+    name: "Chrome Tour",
+    image: "/images/balls-2024-chrome-tour_2___2.webp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    brand: "Golf Proshop",
+    category: "Golf",
+    price: 100,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "2",
     name: "Chrome Tour Triple Track",
     image: "/images/balls-2024-chrome-tour-triple-track-b3g1_2___2.webp",
     description:
@@ -13,24 +26,9 @@ const products = [
     numReviews: 10,
   },
   {
-    _id: "2",
-    name: "Golf Ball",
-    image:
-      "https://images.unsplash.com/photo-1542107491-147e12f03a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    brand: "Golf Proshop",
-    category: "Golf",
-    price: 100,
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 10,
-  },
-  {
     _id: "3",
-    name: "Golf Ball",
-    image:
-      "https://images.unsplash.com/photo-1542107491-147e12f03a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    name: "Chrome Tour Triple Track 360",
+    image: "/images/balls-2024-chrome-tour-360-triple-track_2___2 copy.webp",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     brand: "Golf Proshop",
@@ -42,9 +40,8 @@ const products = [
   },
   {
     _id: "4",
-    name: "Golf Ball",
-    image:
-      "https://images.unsplash.com/photo-1542107491-147e12f03a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    name: "Chrome Tour True Track",
+    image: "/images/balls-2024-chrome-tour-trutrack_6224___2.webp",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     brand: "Golf Proshop",
@@ -56,9 +53,47 @@ const products = [
   },
   {
     _id: "5",
-    name: "Golf Ball",
-    image:
-      "https://images.unsplash.com/photo-1542107491-147e12f03a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    name: "Chrome Tour Yellow",
+    image: "/images/balls-2024-chrome-tour-yellow_1446___2.webp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    brand: "Golf Proshop",
+    category: "Golf",
+    price: 100,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Chrome Tour Triple Track Yellow",
+    image: "/images/balls-2024-chrome-tour-triple-track-yellow_1446___2.webp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    brand: "Golf Proshop",
+    category: "Golf",
+    price: 100,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "7",
+    name: "Chrome Tour Triple Track 360 Yellow",
+    image: "/images/balls-2024-chrome-tour-360-triple-track_1446___2.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    brand: "Golf Proshop",
+    category: "Golf",
+    price: 100,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "8",
+    name: "Chrome Tour True Track Yellow",
+    image: "/images/balls-2024-chrome-tour-trutrack-yellow_1446___2.webp",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     brand: "Golf Proshop",

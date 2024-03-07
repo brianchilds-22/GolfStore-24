75e5b0075b7a21850cfb4a4a9be76059fd7890b4
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-03-07 09:32:15 -0700

    router

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -28,7 +28,7 @@ const products = [
   {
     _id: "3",
     name: "Chrome Tour Triple Track 360",
-    image: "/images/balls-2024-chrome-tour-360-triple-track_2___2 copy.webp",
+    image: "/images/balls-2024-chrome-tour-360-triple-track_2___2.webp",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     brand: "Golf Proshop",

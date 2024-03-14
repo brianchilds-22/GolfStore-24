2c44a6bb1555ac2ea9ae9987114289a9bc3cea4c
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-03-14 13:39:22 -0600

    user-setup-bkend

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  deletetUser,
+  updateUser,
+} from "../controllers/productController.js";
+
+router.route("/").post(registerUser).get(getUsers);
+router.post("/logout", logoutUser);
+router.post("/login", authUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/:id").get(getUserById).delete(deletetUser).put(updateUser);
+
+export default router;

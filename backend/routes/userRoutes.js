a4c5a10d12e49a010c517277f50bd2e50bbf45ff
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-03-15 14:08:44 -0600

    middleware-update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,11 +11,19 @@ import {
   deletetUser,
   updateUser,
 } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(registerUser).get(getUsers);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/logout", logoutUser);
 router.post("/login", authUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
-router.route("/:id").get(getUserById).delete(deletetUser).put(updateUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .get(protect, admin, getUserById)
+  .delete(protect, admin, deletetUser)
+  .put(protect, admin, updateUser);
 
 export default router;

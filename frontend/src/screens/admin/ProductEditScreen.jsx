c6724c37cb36e72767e751c2eac02ea6d6f5f57a
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-03-28 13:26:33 -0600

    ProductListScreen

---
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import FormContainer from "../../components/FormContainer";
+import { toast } from "react-toastify";
+import {
+  useUpdateProductMutation,
+  useGetProductDetailsQuery,
+} from "../../slices/productsApiSlice";
+
+const ProductEditScreen = () => {
+  const { id: productId } = useParams();
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const {
+    data: product,
+    isLoading,
+    refetch,
+    error,
+  } = useGetProductDetailsQuery(productId);
+
+  const [updateProduct, { isLoading: loadingUpdate }] =
+    useUpdateProductMutation();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {}, [product]);
+
+  return <div>ProductEditScreen</div>;
+};
+
+export default ProductEditScreen;

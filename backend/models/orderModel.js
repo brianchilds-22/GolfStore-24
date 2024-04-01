40a5accac5c6c3c65ecdaafed6e7f28d56027437
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-04-01 14:59:50 -0600

    styles

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -27,7 +27,7 @@ const orderSchema = new mongoose.Schema(
         },
         product: {
           type: mongoose.Schema.Types.ObjectId,
-          ref: " Product",
+          ref: "Product",
           required: true,
         },
       },

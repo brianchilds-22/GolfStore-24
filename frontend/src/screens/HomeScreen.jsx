9fee82cc76e5bd5af339c7d429db85a8ea23a330
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-04-01 10:26:21 -0600

    PagesSetup

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,14 @@
 // import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Message from "../components/Message";
+
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -19,7 +22,7 @@ const HomeScreen = () => {
         <>
           <h1>Welcome to the Proshop</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
                 <Product product={product} />
               </Col>

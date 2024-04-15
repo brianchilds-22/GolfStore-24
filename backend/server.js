9059d7ca6bb87d46225d45fc8722a981b0ef0b75
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-04-15 10:03:21 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import path from "path";
 import express from "express";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-dotenv.config();
+// dotenv.config();
 import connectDB from "./config/db.js";
 // import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
